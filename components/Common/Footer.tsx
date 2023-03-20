786a1addcfcc990af66fb2b92663eb5f874c4400
Jaehyeon <jaehyeonp74@gmail.com>
2023-03-20 22:08:43 +0900

    FEAT: 기본 Footer 구현

---
+++ components/Common/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="relative -translate-y-full text-center">
+      <p>
+        <span>Copyright 2023. Jaehyeon. All Rights Reserved.</span>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
