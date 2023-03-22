4b6bda5ca7c5157c7c8a665d51a52221dd726ba9
Jaehyeon <jaehyeonp74@gmail.com>
2023-03-22 15:08:09 +0900

    FEAT: ThemeToggle 구현하기

--- components/Common/NavBar.tsx
+++ components/Common/NavBar.tsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
 import React from "react";
+import ThemeToggle from "./ThemeToggle";
 
 function NavBar() {
   return (
-    <div className="fixed top-0 left-0 right-0 h-20 p-4 flex justify-between items-center z-10">
+    <div className="fixed top-0 left-0 right-0 h-20 p-4 flex justify-between items-center z-10 dark:text-white dark:bg-black">
       <div className="flex-1">
         <Link href={"/"}>재현의 개발블로그</Link>
       </div>
@@ -12,7 +13,9 @@ function NavBar() {
           <li>
             <Link href={"/about"}>About</Link>
           </li>
-          <li>DarkmodeToggle</li>
+          <li>
+            <ThemeToggle />
+          </li>
           <li>SearchBar</li>
         </ul>
       </div>
